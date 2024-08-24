e4a591e0ff69d26362ee747b7bbab5ab65e34325
Umar99-me <68494863+Umar99-me@users.noreply.github.com>
2024-08-24 09:11:05 +0000

    Update java version

--- src/test/java/com/java/stream/problems/numbers/IntegersOnlyTest.java
+++ src/test/java/com/java/stream/problems/numbers/IntegersOnlyTest.java
@@ -2,6 +2,8 @@ package com.java.stream.problems.numbers;
 
 import com.java.stream.solutions.GeneralProblemsNumbersSolution;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -18,7 +20,8 @@ public class IntegersOnlyTest {
         var input = List.of("1","12","12a","a12");
         var actual = List.of();
         var expected = GeneralProblemsNumbersSolution.getNumberOnly(input);
-
+        actual = actual.stream().filter(each -> each.chars()
+        .allMatch(Character::isDigit)).collect(Collectors.toList());
         Assertions.assertEquals(expected, actual);
 
     }
